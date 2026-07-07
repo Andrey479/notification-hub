d9564c19558c435af67c634ce3472c455e23fcd7
Andrey <andreyofbbrasil@gmail.com>
2026-07-07 17:17:43 -0300

    feat: add initial project structure for notification hub service

---
+++ src/test/java/com/andrey/notificationhub/NotificationhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.notificationhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
